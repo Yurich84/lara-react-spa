accb697a8faf9129b434ed2e969c0e84910019c0
Yurii Tymchuk <yurii.tymchuk@skyup.studio>
2019-11-03 13:28:50 +0200

    git fix compiled

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -6,10 +6,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name') }}</title>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('compiled/css/app.css') }}" rel="stylesheet">
     </head>
     <body>
     <div id="root"></div>
-    <script src="{{ asset("js/app.js") }}" charset="utf-8"></script>
+    <script src="{{ asset("compiled/js/app.js") }}" charset="utf-8"></script>
     </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.setPublicPath('public_html/');
+mix.setPublicPath('public_html/compiled/');
 
 mix.react('resources/js/app.js', 'js')
    .sass('resources/sass/app.scss', 'css');
